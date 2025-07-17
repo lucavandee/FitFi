9c6a168320a210f0a5a3f6fba7451fa124ae6980
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-07-17 15:43:45 +0200

    Updated EnhancedResultsPage.tsx

--- src/pages/EnhancedResultsPage.tsx
+++ src/pages/EnhancedResultsPage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState, useCallback } from "react";
+import { USE_MOCK_DATA } from "../config/app-config";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useUser } from "../context/UserContext";
 import { useGamification } from "../context/GamificationContext";
