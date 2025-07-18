f2090fa853c35026afcb478d0726e7361a6b942b
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-07-18 14:07:22 +0200

    Add Google Analytics tracking to FitFi

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { configureRouterFutureFlags } from './utils/routerUtils';
 import { initializeSentry } from './utils/sentryConfig';
+import { initializeAnalytics } from './utils/analytics';
 import './styles/main.css';
 
 // Configure React Router future flags to suppress warnings
@@ -10,6 +11,9 @@ configureRouterFutureFlags();
 
 // Initialize Sentry
 initializeSentry();
+
+// Initialize Google Analytics
+initializeAnalytics();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
