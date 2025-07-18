6ca6bbcc2b0114e1c578213ecb4eb8c0ebaeb8d2
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-07-18 09:22:18 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { configureRouterFutureFlags } from './utils/routerUtils';
 import { initializeSentry } from './utils/sentryConfig';
-import { registerServiceWorker } from './registerServiceWorker';
 import './styles/main.css';
 
 // Configure React Router future flags to suppress warnings
@@ -11,9 +10,6 @@ configureRouterFutureFlags();
 
 // Initialize Sentry
 initializeSentry();
-
-// Register service worker for PWA support
-registerServiceWorker();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
