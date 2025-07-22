1555ef09022bfa745aa27e3bdcc0eafd6196f2e0
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-07-22 22:33:32 +0200

    Fix ESLint jsx-a11y/color-contrast rule error

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,6 @@ export default tseslint.config(
       ],
       'tailwindcss/classnames-order': 'warn',
       'tailwindcss/no-custom-classname': 'warn',
-      'jsx-a11y/color-contrast': ['error', { threshold: 4.5 }],
       'jsx-a11y/focus-visible': 'error',
     },
   }
