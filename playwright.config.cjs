8deb14e5187aadabdf18071db2867df341019ec9
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-07-29 21:28:30 +0200

    Updated playwright.config.cjs

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -1,24 +1,52 @@
-@@ -1,4 +1,5 @@
- // @ts-check
-+const AxeBuilder = require('@axe-core/playwright').default;
- const { defineConfig, devices } = require('@playwright/test');
- 
- /**
-@@ -25,6 +26,9 @@
-     /* Record video on failure */
-     video: 'retain-on-failure',
-     
-+    /* Accessibility testing */
-+    use: { ...devices['Desktop Chrome'], axeBuilder: AxeBuilder },
-+    
-     /* Timeout for each action */
-     actionTimeout: 10000,
-     
-@@ -35,6 +39,7 @@
-   /* Configure projects for major browsers */
-   projects: [
-     {
-       name: 'chromium',
-       use: { ...devices['Desktop Chrome'] },
-     },
-   ]
+// @ts-check
+const AxeBuilder = require('@axe-core/playwright').default;
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    
+    /* Record video on failure */
+    video: 'retain-on-failure',
+    
+    /* Accessibility testing */
+    use: { ...devices['Desktop Chrome'], axeBuilder: AxeBuilder },
+    
+    /* Timeout for each action */
+    actionTimeout: 10000,
+    
+    /* Take screenshot on failure */
+    screenshot: 'only-on-failure'
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  /* Global setup and teardown */
+  globalSetup: require.resolve('./tests/global-setup.cjs'),
+  globalTeardown: require.resolve('./tests/global-teardown.cjs')
+});
