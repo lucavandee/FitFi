20b6e12d0e16bb6d05d4a8bbf6708f5f91c220de
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-07-30 00:16:28 +0200

    Create missing ScrollToTop component

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,24 @@
+import { useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+/**
+ * ScrollToTop component that automatically scrolls to the top of the page
+ * whenever the route changes. Uses useLayoutEffect for immediate scroll
+ * before the browser paints the new content.
+ */
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    // Scroll to top smoothly on route change
+    window.scrollTo({ 
+      top: 0, 
+      behavior: 'smooth' 
+    });
+  }, [pathname]);
+
+  // This component doesn't render anything
+  return null;
+};
+
+export default ScrollToTop;
