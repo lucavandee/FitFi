70a5fef2034cb021b0f910e14b07bf74d4c8e685
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-08-08 13:55:41 +0200

    Remove duplicate ThankYouPage declaration

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ const ThankYouPage = React.lazy(() => import('@/pages/ThankYouPage'));
 // Heavy pages already lazy loaded
 // Heavy pages already lazy loaded
 // Heavy pages already lazy loaded
+// Heavy pages already lazy loaded
 const OnboardingPage = React.lazy(() => import('@/pages/OnboardingPage'));
 const QuizPage = React.lazy(() => import('@/pages/QuizPage'));
 const ResultsPage = React.lazy(() => import('@/pages/ResultsPage'));
@@ -50,7 +51,9 @@ const BlogPage = React.lazy(() => import('@/pages/BlogPage'));
 const BlogPage = React.lazy(() => import('@/pages/BlogPage'));
 const BlogPage = React.lazy(() => import('@/pages/BlogPage'));
 const BlogPage = React.lazy(() => import('@/pages/BlogPage'));
+const BlogPage = React.lazy(() => import('@/pages/BlogPage'));
 const BlogIndexPage = React.lazy(() => import('@/pages/BlogIndexPage'));
+const BlogDetailPage = React.lazy(() => import('@/pages/BlogDetailPage'));
 const BlogDetailPage = React.lazy(() => import('@/pages/BlogDetailPage'));
 const BlogDetailPage = React.lazy(() => import('@/pages/BlogDetailPage'));
 const BlogDetailPage = React.lazy(() => import('@/pages/BlogDetailPage'));
@@ -113,6 +116,8 @@ function App() {
                   </React.Suspense>
                     <NovaBubble />
                   </React.Suspense>
+                    <NovaBubble />
+                  </React.Suspense>
                   </React.Suspense>
                   <React.Suspense fallback={<PageLoadingFallback />}>
                   <Routes>
@@ -123,6 +128,7 @@ function App() {
                     <Route path="/hoe-het-werkt" element={<HowItWorksPage />} />
                     <Route path="/prijzen" element={<PricingPage />} />
                     <Route path="/blog" element={<BlogIndexPage />} />
+                    <Route path="/blog/:slug" element={<BlogDetailPage />} />
                     <Route path="/blog/:slug" element={<BlogDetailPage />} />
                     <Route path="/blog/:slug" element={<BlogDetailPage />} />
                     <Route path="/blog/:slug" element={<BlogDetailPage />} />
