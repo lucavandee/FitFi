1a1f18224a5ddd7111d04061f023c60e25b66ad5
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-08-11 10:15:39 +0200

    Fix engine index exports to remove unused imports

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,29 +1,9 @@
 // Export all engine functionality from a single entry point
 export * from './types';
-export * from './calculateMatchScore';
 export * from './filterAndSortProducts';
-export * from './generateOutfits';
+export { default as generateOutfits } from './generateOutfits';
 export * from './generateOutfitDescriptions';
+export * from './explainOutfit';
 export * from './profile-mapping';
 export * from './recommendationEngine';
-export * from './explainOutfit';
 export * from './helpers';
-
-// Also export as default objects for non-ES6 imports
-import { calculateMatchScore, calculateMatchPercentage } from './calculateMatchScore';
-import { filterAndSortProducts, groupProductsByType, getTopProductsByType } from './filterAndSortProducts';
-import generateOutfits from './generateOutfits';
-import { generateOutfitTitle, generateOutfitDescription } from './generateOutfitDescriptions';
-import { generateOutfitExplanation } from './explainOutfit';
-import { analyzeUserProfile, determineArchetypesFromAnswers, getStyleKeywords } from './profile-mapping';
-import { generateRecommendations, getRecommendedProducts } from './recommendationEngine';
-import { 
-  getCurrentSeason, 
-  getDutchSeasonName, 
-  getProductCategory, 
-  isProductInSeason,
-  isProductSuitableForWeather,
-  getTypicalWeatherForSeason,
-  getDutchWeatherDescription
-} from './helpers';
-
