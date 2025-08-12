2dbd70073a95da47b92f5712c19a2e44de39003e
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-08-13 00:34:07 +0200

    Fix react-helmet-async crash (Cannot read properties of undefined)

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import '@/bootstrap/guards';
+import { HelmetProvider } from 'react-helmet-async';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
@@ -6,6 +7,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
