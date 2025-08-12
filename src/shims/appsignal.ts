2793270bc7b905d43fa0b2e5b95ef41a907f06f3
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-08-13 00:25:15 +0200

    No-White-Screen Hotfix Pack (prod + preview safe)

---
+++ src/shims/appsignal.ts
@@ -0,0 +1,15 @@
+export class Appsignal {
+  constructor(_opts?: any) {}
+  addBreadcrumb(){} 
+  setUser(){} 
+  setContext(){} 
+  instrument(){} 
+  wrap(){} 
+  performance(){} 
+  send(){}
+  setErrorHandler(){} 
+  start(){} 
+  stop(){}
+}
+
+export default Appsignal;
