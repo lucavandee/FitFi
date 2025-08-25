7ca150e79360172c60f24f055041e58bd78158f9
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-08-25 11:45:58 +0200

    Update frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    host: true,
+    open: false,
+    hmr: { overlay: true },
+  },
 });
