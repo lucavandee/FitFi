a73996675080e6eae7ad0d087389ec21c5839851
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-08-29 11:24:37 +0200

    Enable source maps in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+
 import path from "path";
 
 export default defineConfig(({ mode }) => {
@@ -78,5 +79,8 @@ export default defineConfig(({ mode }) => {
   };
   build: {
     sourcemap: true
+  }
+  build: { 
+    sourcemap: true 
   }
 });
