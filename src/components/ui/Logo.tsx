e6c32dcab2e8e835932b73653a7892324b45bb10
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-02 08:33:10 +0200

    Overschrijf Logo.tsx met correcte versie

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -15,10 +15,7 @@ const Logo: React.FC<LogoProps> = ({
     <div className={`flex items-center ${className}`}>
       <div className="relative">
         <Shirt className="text-[#FF8600]" size={24} />
-        <Sparkles
-          className="text-[#0ea5e9] absolute -top-1 -right-1"
-          size={12}
-        />
+        <Sparkles className="text-[#0ea5e9] absolute -top-1 -right-1" size={12} />
       </div>
       <span className={`ml-2 font-display font-semibold text-xl ${textColor}`}>
         <span className="text-[#FF8600]">Fit</span>
