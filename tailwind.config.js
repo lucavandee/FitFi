a66f162db6a49e253c8df2f0d03063dbba7b3aed
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-02 08:29:01 +0200

    Voeg font-display alias toe aan Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        display: ["Montserrat", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
       colors: {
         // FitFi Brand Colors
         'midnight': '#0D1B2A',
