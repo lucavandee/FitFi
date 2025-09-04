62a0ab42efefdf80d33c4619f2a1bec808975e9a
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-04 20:03:04 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ErrorBoundary from "@/components/ErrorBoundary";
 // CrashGate (systeem pad, niet ui/)
 import CrashGate from "@/components/system/CrashGate";
 // HelmetProvider
