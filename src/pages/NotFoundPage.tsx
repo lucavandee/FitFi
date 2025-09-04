067bda9a0d9c2c2d7f12a2cd65dbea2c245fd2fb
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-04 18:21:57 +0200

    Fix SEO import to use default import in NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { SEO } from "@/components/system/SEO";
+import SEO from "@/components/system/SEO";
 
 export default function NotFoundPage() {
   return (
