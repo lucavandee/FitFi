9fa4487e5bd57de7b277a4ffb42c6a54b3d016c4
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-04 19:47:06 +0200

    Restore original main.tsx architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,21 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import AuthProvider from "@/context/AuthContext";
-import { GamificationProvider } from "@/context/GamificationContext";
-import ErrorBoundary from "@/components/ErrorBoundary";
-import CrashGate from "@/components/ui/CrashGate";
 
-createRoot(document.getElementById("root")!).render(
+// Eigen ErrorBoundary
+import ErrorBoundary from "@/components/ErrorBoundary";
+// CrashGate (systeem pad, niet ui/)
+import CrashGate from "@/components/system/CrashGate";
+// HelmetProvider
+import { HelmetProvider } from "react-helmet-async";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <ErrorBoundary>
         <CrashGate>
-          <AuthProvider>
-            <GamificationProvider>
-              <BrowserRouter>
-                <App />
-              </BrowserRouter>
-            </GamificationProvider>
-          </AuthProvider>
+          <App />
         </CrashGate>
       </ErrorBoundary>
     </HelmetProvider>
