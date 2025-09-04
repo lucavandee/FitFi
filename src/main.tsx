e9bcfcf236a53445bbd9917746c0ee43ed5a4c97
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-04 19:48:49 +0200

    Fix conflicts in main.tsx - remove BrowserRouter and fix CrashGate import

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-// Eigen ErrorBoundary
-import ErrorBoundary from "@/components/ErrorBoundary";
 // CrashGate (systeem pad, niet ui/)
 import CrashGate from "@/components/system/CrashGate";
 // HelmetProvider
 import { HelmetProvider } from "react-helmet-async";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <ErrorBoundary>
         <CrashGate>
           <App />
         </CrashGate>
