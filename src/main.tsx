fdfb2558d71ea9620e0903f342c304e4ee9a0c81
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-05 14:21:28 +0200

    HelmetProvider en veilige dev stub

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 
 const rootEl = document.getElementById("root")!;
 createRoot(rootEl).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
