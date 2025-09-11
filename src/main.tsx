7924fecbeb8ef484b8ae6bc0b644d450f222580d
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-11 08:17:29 +0200

    Globale centrering + Helmet + Nova provider/mount

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Helmet provider (vereist voor react-helmet-async <Helmet /> usage)
 import { HelmetProvider } from "react-helmet-async";
-
-// Nova (provider + floating mount)
 import NovaChatProvider from "@/components/nova/NovaChatProvider";
 import NovaChatMount from "@/components/nova/NovaChatMount";
 
@@ -15,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <NovaChatProvider>
-        {/* Globale, consistente centrering voor alle routes */}
+        {/* Globale, consistente centrering */}
         <div className="min-h-screen bg-[#F6F6F6]">
           <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
             <App />
