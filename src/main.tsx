9482787fb4a10a8319750093d521181c243d4a45
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-12 10:33:49 +0200

    Fix dubbele Router - BrowserRouter uit main.tsx verwijderen

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 // /src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// ❌ Geen BrowserRouter hier (App bevat al een Router)
 import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
-// ⬇️ Named import omdat NovaChatProvider geen default export heeft
 import { NovaChatProvider } from "@/components/nova/NovaChatProvider";
 import NovaChatMount from "@/components/nova/NovaChatMount";
 
@@ -15,13 +14,11 @@ import "./components/nova/boot.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <NovaChatProvider>
-          <App />
-          {/* Floating FAB + overlay panel (rechtsonder) */}
-          <NovaChatMount />
-        </NovaChatProvider>
-      </BrowserRouter>
+      <NovaChatProvider>
+        <App />
+        {/* Floating FAB + overlay panel (rechtsonder) */}
+        <NovaChatMount />
+      </NovaChatProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
