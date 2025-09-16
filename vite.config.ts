f38fd25c7fb43e22b5ff8d09be1a0af784c05914
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-16 11:22:59 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 // vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 
 export default defineConfig(({ command }) => {
   const isDev = command === "serve";
@@ -51,7 +52,7 @@ export default defineConfig(({ command }) => {
     ].filter(Boolean),
     resolve: {
       alias: {
-        "@": new URL("./src", import.meta.url).pathname
+        "@": path.resolve(__dirname, "src")
       }
     },
     server: { port: 5173, strictPort: false },
