7996763f5b1573d7ed61ba12d30e5af1f47a6e98
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-16 14:28:18 +0200

    Fix main.tsx and index.html to make existing files visible

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,17 @@
-// /src/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-// Geen BrowserRouter hier (App bevat al een Router)
-import { HelmetProvider } from "react-helmet-async";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-import App from "./App";
-import { NovaChatProvider } from "@/components/nova/NovaChatProvider";
-import NovaChatMount from "@/components/nova/NovaChatMount";
-import "@/index.css";
+// Bootstrap guards en analytics
+import './bootstrap/guards'
+import { initAnalytics } from './utils/analytics'
 
-// CSS: eerst basis, DAN polish als allerlaatste (zorgt dat polish alles kan overrulen)
-import "./index.css";
-import "./styles/polish.css";
+// Initialize analytics
+initAnalytics()
 
-// Boot Nova eenmaal (dubbele import verwijderd)
-import "./components/nova/boot.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <NovaChatProvider>
-        <App />
-        {/* Floating FAB + overlay panel (rechtsonder) */}
-        <NovaChatMount />
-      </NovaChatProvider>
-    </HelmetProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
