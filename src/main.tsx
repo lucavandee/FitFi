56140407d759fda281934507490efcadd0cde1fd
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-20 20:11:53 +0200

    Implement CSS build configuration with tokens-first styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,15 @@
-// /src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Geen BrowserRouter hier (App bevat al een Router)
-import { HelmetProvider } from "react-helmet-async";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { NovaChatProvider } from "@/components/nova/NovaChatProvider";
-import NovaChatMount from "@/components/nova/NovaChatMount";
 
-// CSS: eerst basis, DAN polish als allerlaatste (zorgt dat polish alles kan overrulen)
-import "./index.css";
-import "./styles/polish.css";
-
-// Boot Nova eenmaal (dubbele import verwijderd)
-import "./components/nova/boot.tsx";
+// BELANGRIJK: globale styles (Tailwind + tokens + polish)
+import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <NovaChatProvider>
-        <App />
-        {/* Floating FAB + overlay panel (rechtsonder) */}
-        <NovaChatMount />
-      </NovaChatProvider>
-    </HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
