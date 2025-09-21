9327c839c69af1a5f2a69d6527564244376c9125
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-21 14:17:52 +0200

    Fix import errors in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import ThemeProvider from "@/context/ThemeContext";
+import UserProvider from "@/context/UserContext";
+import GamificationProvider from "@/context/GamificationContext";
+import OnboardingProvider from "@/context/OnboardingContext";
+import NovaChatProvider from "@/components/nova/NovaChatProvider";
 import App from "./App";
 import "./index.css";
 
