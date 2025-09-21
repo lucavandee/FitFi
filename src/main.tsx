d52f96742020e1dda982ce61e411097c757ac446
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-21 14:18:40 +0200

    Updated App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
-import "./index.css";
+
+// BELANGRIJK: globale styles (Tailwind + tokens + polish)
+import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
