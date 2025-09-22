2f5a3c1011daebb18f0a1faf2148f35ce63f9094
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-22 16:14:12 +0200

    Updated NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const NotFoundPage = () => {
+  return (
     <section className="ff-section bg-white">
       <div className="ff-container">
         <h1 className="ff-h1">Pagina niet gevonden</h1>
