4ce1266ae20b81ea8aa8b578d120ffc236e751e1
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-22 13:50:40 +0200

    Fix syntax error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,14 @@
-// /src/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-// Geen BrowserRouter hier (App bevat al een Router)
-import { HelmetProvider } from "react-helmet-async";
-import "@/styles/polish.css";
-
-import App from "./App";
-import { NovaChatProvider } from "@/components/nova/NovaChatProvider";
-import NovaChatMount from "@/components/nova/NovaChatMount";
-
-// CSS: eerst basis, DAN polish als allerlaatste (zorgt dat polish alles kan overrulen)
-import "./index.css";
-import "./styles/polish.css";
-
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// Boot Nova eenmaal (dubbele import verwijderd)
-import "./components/nova/boot.tsx";
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <NovaChatProvider>
-        <App />
-        {/* Floating FAB + overlay panel (rechtsonder) */}
-        <NovaChatMount />
-      </NovaChatProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-)
