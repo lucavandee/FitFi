a4dba542f2fe30f7fb16fcfbec0678e75445a804
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-23 15:58:51 +0200

    Fix Vite and TypeScript alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 // vite.config.ts
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -12,7 +13,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     resolve: {
-      alias: { "@": "/src" },
+      alias: { "@": path.resolve(__dirname, './src') },
     },
     server: { host: true, port: DEV_PORT, strictPort: true },
     preview: { host: true, port: PREVIEW_PORT },
