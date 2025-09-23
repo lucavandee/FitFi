8eecc134e950806139f7765982fe6150f15c838f
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-23 16:06:54 +0200

    Wrap app met HelmetProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import "@/styles/index.css";
 
-// CSS imports in correct order
-import '@/styles/index.css'
-
-// Header/Footer polish & overrides
-import '@/styles/polish/95-header-footer.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
