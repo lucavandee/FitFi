40cc9d2473e2f27261824805e25ed6ae14122e2a
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-09-23 16:27:31 +0200

    Split CSS op in logische modules

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
-import "@/styles/index.css";
+import "@/styles/polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
