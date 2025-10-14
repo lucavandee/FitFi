bce0a951df04785714b5e9343232d088698ac4a8
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2025-10-14 11:16:16 +0200

    Updated hash.ts

--- src/utils/hash.ts
+++
@@ -1,5 +0,0 @@
-export function hashString(input: string): number {
-  let h = 2166136261 >>> 0;
-  for (let i = 0; i < input.length; i++) { h ^= input.charCodeAt(i); h = Math.imul(h, 16777619); }
-  return h >>> 0;
-}
