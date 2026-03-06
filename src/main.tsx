254828a05a79b36da845b08dc27b3b7db6112ff8
Lucaavd <55696751+lucavandee@users.noreply.github.com>
2026-03-06 07:55:30 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App";
-import AnalyticsLoader from "@/components/analytics/AnalyticsLoader";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import "./index.css";               // Tailwind + tokens
 import "@/styles/polish.addon.css"; // opt-in polish
@@ -53,7 +52,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 <GamificationProvider>
                   <OnboardingProvider>
                     <App />
-                    <AnalyticsLoader />
                     <Toaster position="top-center" />
                   </OnboardingProvider>
                 </GamificationProvider>
